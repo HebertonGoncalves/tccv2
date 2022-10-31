3e9d15d40b993aa57433bbd6e66e5834d03b8980
Heberton <hebertontecno@hotmail.com>
2022-10-31 08:34:27 -0300

    atualização nos itens da lista agora com onPress

--- src/components/ModeloConteudos.js
+++ src/components/ModeloConteudos.js
@@ -1,18 +1,20 @@
 import * as React from 'react';
 import { List } from 'react-native-paper';
- 
+import { useNavigation } from '@react-navigation/native';
+
+
  const MyComponent  = () => {
   const [expanded, setExpanded] = React.useState(true);
 
   const handlePress = () => setExpanded(!expanded);
-
+  const navigation = useNavigation(); 
   return (
     <List.Section title="Conteudos">
       <List.Accordion
         title="Videos"
         left={props => <List.Icon {...props} icon="folder" />}>
-        <List.Item title="Videoaula 1" />
-        <List.Item title="Videoaula 2" />
+        <List.Item onPress={() => navigation.navigate("TelaIntroducao")} title="Videoaula 1" />
+        <List.Item onPress={() => navigation.navigate("home")} title="Videoaula 2" />
       </List.Accordion>
 
       <List.Accordion
