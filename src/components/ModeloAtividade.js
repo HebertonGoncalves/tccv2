f8d533f5bfe260f52216b15ffa8c85c181ba19b1
Heberton <hebertontecno@hotmail.com>
2023-04-28 10:20:07 -0300

    continuar em casa

---
+++ src/components/ModeloAtividade.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, SafeAreaView, Text, Image, View, TouchableHighlight, Linking } from 'react-native';
+
+export default function ModeloAtividade({navigation}) {}
+
+const styles = StyleSheet.create({
+    
+})
