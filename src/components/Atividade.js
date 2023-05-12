e21a9e98e093ead9f02ebb11f977796b6788bd53
Heberton <hebertontecno@hotmail.com>
2023-05-12 15:10:33 -0300

    tentando inserir a resposta correta

--- src/components/Atividade.js
+++ src/components/Atividade.js
@@ -1,23 +1,41 @@
-//Modelo de tela para atividades
-
-import { SafeAreaView,TouchableOpacity, ScrollView, StyleSheet, Text, Touchable, View } from "react-native"
-import { Button } from "react-native-paper";
+import { SafeAreaView,TouchableOpacity, ScrollView, StyleSheet, Text, View } from "react-native"
 import * as React from 'react';
 import { RadioButton } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Atividade(){
   const [checked, setChecked] = React.useState('first');
+
+
+  const Armazenar = async (chave, valor) => {
+    try {
+      const jsonValue = JSON.stringify(valor)
+      await AsyncStorage.setItem(chave, jsonValue)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  function correta (){
+      if(checked=='primeiro'){
+        Armazenar('1', 1)
+      }else{Armazenar('1', 0)}
+  }
+
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView style={styles.scrollView}>
               <View style={styles.View}>
-                <Text>Qual das alternativas representa a entrada de um sistema ?</Text>
-                <RadioButton.Item label="teclado" value="primeiro" status={ checked === 'primeiro' ? 'checked' : 'unchecked' } onPress={() => setChecked('primeiro')}/>
-                <RadioButton.Item label="impressora" value="segundo" status={ checked === 'segundo' ? 'checked' : 'unchecked' } onPress={() => setChecked('segundo')}/>
-                <RadioButton.Item label="tela" value="terceiro" status={ checked === 'terceiro' ? 'checked' : 'unchecked' } onPress={() => setChecked('terceiro')}/>
-                <RadioButton.Item label="fax" value="quarto" status={ checked === 'quarto' ? 'checked' : 'unchecked' } onPress={() => setChecked('quarto')}/>
+                <Text style={styles.texto}>Hoje, cada vez mais, as empresas que atuam no ramo de software têm adotado o termo "Fábrica de Software", seja em virtude 
+                  da alta demanda de software no mercado, seja o aumento crescente da complexidade dos softwares, 
+                  seja como uma solução para produzir seus produtos ou serviços com maior qualidade, maior produtividade e baixo custo de produção.
+                </Text>
+                <RadioButton.Item style={styles.radio} label="Associa os processos do Feudalismo e Fordismo para engajar profissionais da Tecnologia da Informação." value="primeiro" status={ checked === 'primeiro' ? 'checked' : 'unchecked' } onPress={() => setChecked('primeiro')}/>
+                <RadioButton.Item style={styles.radio} label="É cada vez mais adotado para solucionar as demandas de software relacionados à automação industrial." value="segundo" status={ checked === 'segundo' ? 'checked' : 'unchecked' } onPress={() => setChecked('segundo')}/>
+                <RadioButton.Item style={styles.radio} label="Permite o aumento da qualidade, da produtividade e padronizar as atividades de desenvolvimento de software." value="terceiro" status={ checked === 'terceiro' ? 'checked' : 'unchecked' } onPress={() => setChecked('terceiro')}/>
+                <RadioButton.Item style={styles.radio} label="Solução encontrada para a dificuldade de encontrar mão de obra especializada no desenvolvimento de software." value="quarto" status={ checked === 'quarto' ? 'checked' : 'unchecked' } onPress={() => setChecked('quarto')}/>
               </View> 
-              <TouchableOpacity style={styles.cartoes}><Text>Responder</Text></TouchableOpacity>
+              <TouchableOpacity onPress={correta()} style={styles.botao}><Text>Responder</Text></TouchableOpacity>
             </ScrollView>
         </SafeAreaView>
     )
@@ -29,12 +47,12 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
-      padding:0
+      padding:1
     },
     scrollView: {
       backgroundColor: 'white',
       marginHorizontal: 20,
-      padding:10,
+      padding: 10,
       margin: 5
     },
     View: {
@@ -47,14 +65,30 @@ const styles = StyleSheet.create({
       marginHorizontal: 15,
       marginBottom: 10,
     },
-      cartoes:{
-      width: 350, 
+      botao:{
+      width: 330, 
       height: 80, 
       position: "relative", 
       justifyContent: "center", 
       alignItems: "center",
       borderRadius: 1,
-      backgroundColor: "#5c677d",
-      borderBottomWidth:1,
+      backgroundColor: "#0466c8",
+      borderWidth:1,
+      margin:2,
+      marginLeft: 8,
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    radio:{
+      borderWidth: 1,
+      margin: 2,
+      marginLeft: 8,
+      height: 100,
+      width: 330,
+    },
+    texto:{
+      marginBottom: 10,
+      fontSize: 16,
+      fontStyle: "italic",
     }
   });
--- src/components/ModeloAtividade.js
+++ src/components/ModeloAtividade.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, SafeAreaView, Text, Image, View, TouchableHighlight, Linking } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function ModeloAtividade({navigation}) {
 
+  const [result, setResult] = useState(null)
+
+  const Buscar = async (chave) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(chave)
+      var result = jsonValue != null ? JSON.parse(jsonValue) : null;
+      setResult(result)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+        Buscar('1')
+
     return (
             <SafeAreaView style={styles.container}>
-                <TouchableOpacity style={styles.cartoes}>
+                <TouchableOpacity style={styles.cartoes} onPress={()=>navigation.navigate('Atividade')}>
                     <Text style={styles.nota}>2.5</Text>
                   <View style= {{flexDirection:'column'}}>
                     <Text style={styles.titulo}>Questão 1</Text>
@@ -17,22 +31,23 @@ export default function ModeloAtividade({navigation}) {
                     <Text style={styles.nota}>2.5</Text>
                   <View style= {{flexDirection:'column'}}>
                     <Text style={styles.titulo}>Questão 2</Text>
-                    <Text style={styles.texto}>Qual a unidade de medida da corrente ?</Text>
+                    <Text style={styles.texto}>Qual a unidade de medida da tensão ?</Text>
                   </View>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.cartoes}>
                     <Text style={styles.nota}>2.5</Text>
                   <View style= {{flexDirection:'column'}}>
                     <Text style={styles.titulo}>Questão 3</Text>
-                    <Text style={styles.texto}>Qual a unidade de medida da corrente ?</Text>
+                    <Text style={styles.texto}>Qual a unidade de medida da resistencia ?</Text>
                   </View>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.cartoes}>
                     <Text style={styles.nota}>2.5</Text>
                   <View style= {{flexDirection:'column'}}>
                     <Text style={styles.titulo}>Questão 4</Text>
-                    <Text style={styles.texto}>Qual a unidade de medida da corrente ?</Text>
+                    <Text style={styles.texto}>Qual a unidade de medida da potencia ?</Text>
                   </View>
+                  <View><Text>resultada: {result}</Text></View>
                 </TouchableOpacity>
             </SafeAreaView>
     );
